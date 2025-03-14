b254087041c87d1d682482a4df352603bb5cd77e
Pracheta kamal <143746703+pracheta24@users.noreply.github.com>
2025-03-14 22:13:05 +0530

    Add files via upload

---
+++ 02_ Anagram Check/AnagramChecker.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramChecker {
+    public static boolean areAnagrams(String str1, String str2) {
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+         char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+         for (int i = 0; i < charArray1.length; i++) {
+            if (charArray1[i] != charArray2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String str1 = scanner.nextLine();
+        System.out.print("Enter second string: ");
+        String str2 = scanner.nextLine();
+        scanner.close();
+        
+        if (areAnagrams(str1, str2)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
